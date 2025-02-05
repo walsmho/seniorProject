dd80db535bf396b1c2c396a513b4e01896f44840
Carl Jones <jones.ryan.t.01@gmail.com>
2025-02-05 15:02:55 -0800

    opened and closed first openCV image

---
+++ learning/cameraTests.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import sys
+
+debug = True
+
+#load image, run test
+testImg = cv.imread("learning\\theSun.jpg")
+if testImg is None and debug == True:
+    sys.exit("cameraTests error: could not read image {}".format(testImg))
+
+cv.imshow("Press to close", testImg)
+#0 = wait forever
+key = cv.waitKey(0)
+
+#Save if s key is pressed
+if key == ord("s"):
+    cv.imwrite("theSun.jpg", testImg)
