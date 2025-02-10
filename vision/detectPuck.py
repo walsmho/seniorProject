6ab6bf156ee775b77dbfd7c706352278e9f7df5a
Carl Jones <jones.ryan.t.01@gmail.com>
2025-02-10 09:13:01 -0800

    implemented objMove function as stepping stone to predict vector. Returns left/right and up/down movement based on bbox coordinate

--- vision/detectPuck.py
+++ vision/detectPuck.py
@@ -1,23 +1,38 @@
 import cv2
 from PIL import Image
-from visionUtil import getLimits, createMask, createBoundingBox
+from visionUtil import getLimits, createMask, createBoundingBox, objMoved
 
-capture = cv2.VideoCapture(1) # for external webcam hookup in setup
-puckColor = [0, 165, 255] # BGR. Using orange for testing
+capture = cv2.VideoCapture(0) # for external webcam hookup in setup
+
+#Temp until permanent setting with good lighting exists
+puckColor = [0,165,255] #BGR orange value
+
+(x1,y1), (x2,y2) = (0,0), (0,0)
+
 debug = True
 
 while True:
-    #Capture frame
+    #Capture frame, create mask
     ret, view = capture.read()
-
     if not ret and debug:
         print("detectPuck error: Can't recieve frame. (stream end?) exiting...")
         break
-
     lowerLimit, upperLimit = getLimits(puckColor)
     colorMaskP = createMask(view, lowerLimit, upperLimit)
 
-    (x1,y1), (x2, y2) = createBoundingBox(view, colorMaskP)
+    #check if object exists
+    boundingInitial = colorMaskP.getbbox()
+
+    #When seeing puck:
+    if boundingInitial is not None:
+        (x1_,y1_), (x2, y2) = createBoundingBox(view, colorMaskP)
+
+        movedX, movedY, trajectoryX, trajectoryY = objMoved((x1,y1),(x1_,y1_), 20, debug)
+
+    #When not seeing puck:
+    elif boundingInitial is None:
+        if debug:
+            print("WARNING: No puck exists in frame")
 
     #Visualize frame
     if debug:
@@ -25,6 +40,9 @@ while True:
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    #Prev coord now = new coord
+    (x1,y1) = (x1_,y1_)
 
 capture.release()
 cv2.destroyAllWindows()
--- vision/visionUtil.py
+++ vision/visionUtil.py
@@ -65,6 +65,7 @@ def createBoundingBox(view, colorMask, boundingBoxColor=(0,0,255), boundingBoxTh
             colorMask (PIL object): PIL image with desired color
             boundingBoxColor (tuple): BRG value of bbox color
             boundingBoxThickness (int): pixel width of bbox
+            debug (bool): Enter deubg mode
 
         Returns:
             x1, y1 (tuple): first corner of bbox coords
@@ -85,4 +86,64 @@ def createBoundingBox(view, colorMask, boundingBoxColor=(0,0,255), boundingBoxTh
         return (x1, y1), (x2, y2)
     else:
         return None
+
+def objMoved(coordPrev, coordNew, tolerance, debug=False):
+    """Check to see if object in bounding box moved and where, given some tolerance
+
+        Args:
+            coordPrev (tuple): Previous coordinate set from last frame
+            coordNew (tuple): Current coordinate set from existing frame
+            tolerance (int): Tolerance of pixels for movement
+            debug (bool): Enter debug mode
+
+        Returns:
+            moved (bool): Whether object moved or not
+            trajectoryX (string): If moved, vector of direction. Else, returns none. Currently returning a string of "left, right" for testing
+            trajectoryY (string): If moved, vector of direction. Else, returns none. Currently returning a string of "up, down" for testing
     
+    """
+    #Check x-axis coords
+    if np.abs(coordPrev[0]-coordNew[0]) > tolerance:
+        movedX = True
+
+        if coordPrev > coordNew:
+            if debug:
+                print("objMoved: Moved Left")
+            trajectoryX = "left"
+        elif coordPrev < coordNew:
+            if debug:
+                print("objMoved: Moved Right")
+            trajectoryX = "right"
+
+    elif np.abs(coordPrev[0]-coordNew[0] <= tolerance):
+        if debug:
+            print("objMoved: No x-axis movement")
+        movedX = False
+        trajectoryX = None
+
+    #Check y-axis coords
+    if np.abs(coordPrev[1]-coordNew[1]) > tolerance:
+        movedY = True
+
+        if coordPrev > coordNew:
+            if debug:
+                print("objMoved: Moved Up")
+            trajectoryY = "up"
+        elif coordPrev < coordNew:
+            if debug:
+                print("objMoved: Moved Down")
+            trajectoryY = "down"
+
+    elif np.abs(coordPrev[1]-coordNew[1] <= tolerance):
+        if debug:
+            print("objMoved: No y-axis movement")
+        movedY = False
+        trajectoryY = None
+
+    else:
+        movedX = False
+        movedY = False
+        trajectoryX = None
+        trajectoryY = None
+    
+    return movedX, movedY, trajectoryX, trajectoryY
