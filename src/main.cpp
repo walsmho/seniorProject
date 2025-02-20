9ea7e2ba454354550dd49b998c700d855a221f81
Carl Jones <jones.ryan.t.01@gmail.com>
2025-02-20 15:08:30 -0800

    initial commit for arduino env setup using platformio

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+// // put function declarations here:
+// int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
+}
+
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
