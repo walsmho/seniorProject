5187aeef48f310912fb40621ce8019fe1925ad03
steel-wing <dw395@byu.edu>
2025-05-11 00:29:50 -0600

    follow along

--- src/motorControl/motorMove.cpp
+++ src/motorControl/motorMove.cpp
@@ -79,6 +79,30 @@ void xLeft(int steps, int delay) {
     }
 }
 
+
+/*  plotLine(x0, y0, x1, y1)
+        dx = abs(x1 - x0)
+        sx = x0 < x1 ? 1 : -1
+        dy = -abs(y1 - y0)
+        sy = y0 < y1 ? 1 : -1
+        error = dx + dy
+        
+        while true
+            plot(x0, y0)
+            e2 = 2 * error
+            if e2 >= dy
+                if x0 == x1 break
+                error = error + dy
+                x0 = x0 + sx
+            end if
+            if e2 <= dx
+                if y0 == y1 break
+                error = error + dx
+                y0 = y0 + sy
+            end if
+        end while
+*/
+
 // deltaX, deltaY: absolute step counts along X and Y
 // sx, sy: +1 or -1 for X and Y directions
 void bresenhamMove(long deltaX, long deltaY, int sx, int sy) {
